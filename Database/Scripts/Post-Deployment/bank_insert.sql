195c1d7e0d56cbf5c2b24379769b31c152765411
Michael Melena <michael.melena@gmail.com>
2019-03-14 10:05:42 +0100

    edit: changed  shortcut for bank

--- Database/Scripts/Post-Deployment/bank_insert.sql
+++ Database/Scripts/Post-Deployment/bank_insert.sql
@@ -36,10 +36,10 @@ IF NOT EXISTS(SELECT * FROM [dbo].[bank] WHERE [name] = N'Raiffeisen Bank') BEGI
 END
 GO
 
-IF NOT EXISTS(SELECT * FROM [dbo].[bank] WHERE [name] = N'Česká spořitelna') BEGIN
+IF NOT EXISTS(SELECT * FROM [dbo].[bank] WHERE [name] = N'Česká pojišťovna') BEGIN
 	INSERT [dbo].[bank]([name], [short_Name], [description]) VALUES(
-		N'Česká spořitelna',
-		N'ČS',
+		N'Česká pojišťovna',
+		N'ČP',
 		null
 	)
 END
